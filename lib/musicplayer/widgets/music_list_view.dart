8e086adaf85fd9da65a36ce381d1a6aa3d674de2
Ronson Kwok <rk6866@hotmail.com>
2023-02-05 16:50:48 +0800

    amended class name

--- lib/musicplayer/widgets/music_list_view.dart
+++ lib/musicplayer/widgets/music_list_view.dart
@@ -2,22 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:musicapp/musicplayer/controllers/music_list_controller.dart';
 
-class SomeWidget extends StatefulWidget {
-  const SomeWidget({super.key});
+class MusicListView extends StatefulWidget {
+  const MusicListView({super.key});
 
   @override
-  State<SomeWidget> createState() => _SomeWidgetState();
+  State<MusicListView> createState() => _MusicListViewState();
 }
 
-class _SomeWidgetState extends State<SomeWidget> {
+class _MusicListViewState extends State<MusicListView> {
   @override
   Widget build(BuildContext context) {
     // final controller = MusicListController
     // controller.musicItems.
     return Obx(() {
       return ListView(
+          padding: const EdgeInsets.all(16),
           children: MusicListController.to.musicItems
-              .map((item) => ListTile(title: Text(item.trackName)))
+              .map((item) => ListTile(
+                    title: Text(
+                      item.trackName,
+                    ),
+                    trailing: const Icon(Icons.play_arrow),
+                    onTap: () {},
+                  ))
               .toList());
     });
   }
