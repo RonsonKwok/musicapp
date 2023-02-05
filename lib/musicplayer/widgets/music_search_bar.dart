c2e308edba887dccce9efa2c904d05d809483dd8
Ronson Kwok <rk6866@hotmail.com>
2023-02-06 00:32:51 +0800

    added clear search button

--- lib/musicplayer/widgets/music_search_bar.dart
+++ lib/musicplayer/widgets/music_search_bar.dart
@@ -6,10 +6,19 @@ class MusicSearchBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    TextEditingController searchtext = TextEditingController();
+
     return TextField(
+      controller: searchtext,
       textAlign: TextAlign.left,
       decoration: InputDecoration(
         hintText: 'Enter a song or artist name to search',
+        suffixIcon: IconButton(
+          icon: const Icon(Icons.clear),
+          onPressed: () {
+            searchtext.clear();
+          },
+        ),
         contentPadding: EdgeInsets.all(20.0),
       ),
       onChanged: MusicListController.to.searchKey,
